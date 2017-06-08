73371ce75fcd231aca4e867d12f239254b60950e
Edgar Y. Walker <edgar.walker@gmail.com>
2017-06-08 02:35:11 -0500

    Add tutorial data for MATLAB

--- _helper/gen_data.py
+++ _helper/gen_data.py
@@ -1,14 +1,14 @@
-def gen_data(file, N=None, min_n=4, max_n=10):
-    if N is None:
-        N = np.random.randint(min_n, max_n)
-    r = []
-    kern = np.exp(-np.arange(30)/10)
-    for i in range(N):
-        x = np.abs(np.random.randn(1000))
-        spikes = x > 2
-        y = np.convolve(spikes, kern, mode='same')
-        r.append(y)
-    r = np.vstack(r)
-    r = r.squeeze()
-    np.save(file, r, allow_pickle=False)
-    
+import numpy as np
+
+def gen_data(file=None, bins=1000, min_spikes=10, max_spikes=50, 
+        kern_width=30, decay_factor=5, peak=1, noise_std=0.1):
+    kern = peak * np.exp(-np.arange(kern_width)/decay_factor)
+    spikes = np.zeros(bins)
+    count = np.random.randint(min_spikes, max_spikes)
+    idx = np.random.choice(bins, count, replace=False)
+    spikes[idx] = 1
+    r  = np.convolve(spikes, kern, mode='same')
+    r += np.random.randn(bins) * noise_std
+    if file is not None:
+        np.save(file, r, allow_pickle=False)
+    return r, count
