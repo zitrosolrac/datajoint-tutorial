b39eb63d6dcfedff0014515f760d4106de835473
Carlos <carlosortiz9204@gmail.com>
2022-04-21 14:54:16 -0500

    working search with stylesheet

--- src/conf.py
+++ src/conf.py
@@ -43,9 +43,23 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+
+# html_theme = "sphinx_rtd_theme"
+
+html_theme = "datajoint_theme"
+html_theme_path = ["."]
+html_logo = './_static/img/djTutorialLogoWhite.svg'
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': False,
+    'logo_only': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
+
+def setup(app):
+    # Custom CSS
+    app.add_stylesheet('css/datajoint_theme.css')
